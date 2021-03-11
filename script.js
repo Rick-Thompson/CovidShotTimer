3c7bfa6507b75f490d2164188b8ced08a7cedaf8
Desktop\Rick <rick.thompson@southernwv.edu>
2021-03-11 10:21:34 -0500

    clean up main page

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
+var username;
+var password;
+// Create a client instance: Broker, Port, Websocket Path, Client ID
+client = new Paho.MQTT.Client("rick50.scorchedsilicon.com", Number(9099), "clientId" + Math.random());
+
+
 window.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById("save").addEventListener("click", function () {
         localStorage.setItem("username", document.getElementById("user").value);
         localStorage.setItem("password", document.getElementById("pass").value);
         document.getElementById("save").innerHTML = "Saved";
+        username = localStorage.getItem("username");
+        password = localStorage.getItem("password");
+        document.getElementById("out").innerHTML = "Invalid user/pass";
+        conn();
     });
 
-    //  var username = localStorage.getItem("username");
-    //  var password = localStorage.getItem("password");
 
 
 });
+
+function conn() {
+    client.connect({
+        onSuccess: onConnect,
+        useSSL: true,
+        userName: username,
+        password: password,
+        mqttVersion: 3
+    });
+}
+
+
+
+// set callback handlers
+client.onConnectionLost = function (responseObject) {
+    console.log("Connection Lost: " + responseObject.errorMessage);
+}
+
+client.onMessageArrived = function (message) {
+    //    console.log("Message Arrived: " + message.payloadString);
+    
+    //    console.log("Topic: " + topic);
+ 
+}
+
+// Called when the connection is made
+function onConnect() {
+    console.log("Connected");
+    document.getElementById("out").innerHTML = "Logged in successfully!";
+}
+
+// Connect the client, providing an onConnect callback
+
